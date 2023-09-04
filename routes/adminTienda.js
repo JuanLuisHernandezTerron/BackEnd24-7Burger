a0f3e0dfd0726ebf45203197d62318bba37b92ba
unknown <you@example.com>
2023-09-05 00:53:12 +0200

    cambios

--- routes/adminTienda.js
+++ routes/adminTienda.js
@@ -3,7 +3,6 @@ var router = express.Router();
 var mongoose = require('mongoose');
 var db = mongoose.connection;
 const { newTienda, loginTienda } = require('./../controllers/adminTiendaController');
-const validatorToken = require('../../24-7Burger/middleware/tokenValidator')
 
 router.post('/newTienda', newTienda);
 router.post('/loginTienda', loginTienda);
